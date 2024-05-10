b925e9be8f9ff63eb3ee5708301c05c35fdbd282
Pavel_Varanovich <pavel_varanovich@epam.com>
2024-05-10 23:21:11 +0200

    WI-11 [BE] change run script

--- src/middleware/utils.ts
+++ src/middleware/utils.ts
@@ -1,7 +1,7 @@
 import { NextFunction } from 'express';
 import { ValidatedRequest } from 'express-joi-validation';
 
-import { AUTH, ERROR_MESSAGES } from '../const';
+import { AUTH, ERROR_MESSAGES } from '@const';
 import { ForbiddenError } from '@errors';
 import { AuthorizationRequestSchema } from '@validation';
 
@@ -13,7 +13,7 @@ export const getToken = (
     next(new ForbiddenError(ERROR_MESSAGES.MUST_AUTHORIZED_USER));
   }
 
-  const [tokenType, token] = req.headers[AUTH.AUTHORIZATION_HEADER].split(' ');
+  const [tokenType, token] = req.headers[AUTH.AUTHORIZATION_HEADER]?.split(' ');
 
   if (tokenType !== AUTH.BEARER) {
     next(new ForbiddenError(ERROR_MESSAGES.INVALID_TOKEN));
